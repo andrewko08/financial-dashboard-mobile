320ce522bb368ec18e4960cdbc2fede1db6352b3
andrewko08 <ko.andrew.08@gmail.com>
2021-09-28 17:20:36 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+<body>
+
+<section class="header">
+    <div>
+        <img src="../img/time.svg">
+    </div>
+    <div>
+        <img src="../img/signal.svg">
+        <img src="../img/wifi.svg">
+        <img src="../img/battery.svg">
+    </div>
+</section>
+
+<!-- FINANCIAL SECTION -->
+
+<section class="financial-section-wrapper">
+    <div class="amount-container padded">
+        <h2>TOTAL SALDO</h2>
+        <h1>Rp 50.000.000,00</h1>
+    </div>
+    <div class="add-button-container">
+        <img src="../img/arrow-right.png">
+        <div class="btn red-btn"><h2>Tambah<h2></div>
+    </div>
+</section>
+
+
+<!-- TRANSACTION CARD SECTION -->
+
+<section class="transaction-section-wrapper padded">
+    <div class="transaction-card-container">
+        <!-- HEADER PART -->
+        <div class="tc-header-container padded">
+            <h4>Transaksi</h4>
+            <div class="two-btns">
+                <div class="btn all-btn"><h4>Semua</h4></div>
+                <div class="btn filter-btn">
+                    <h4>Filter</h4>
+                    <embed src="../img/options.svg">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="seperator"></div>
+        <!-- AMOUNT PART -->
+        <div class="tc-date-amount-container padded">
+            <h4>14 Juli 2021</h4>
+            <div class="tc-amount">
+                <h2>PENDAPATAN KOTOR</h2>
+                <h1>Rp 30.000.000,00</h1>
+            </div>
+        </div>
+
+        <!-- LIST OF TRANSACTIONS -->
+        <div class="tc-list padded">
+
+            <!-- ITEM 1 -->
+            <div class="tc-list-item">
+                <div class="tc-pic-name">
+                    <img src="../img/person-2.png">
+                    <div class="tc-list-desc-name">
+                        <h4>LPG 15 Kg</h4>
+                        <h3>Alexander Riuji</h3>
+                    </div>
+                </div>
+                
+                <div class="tc-label-price">
+                    <h4>Harga</h4>
+                    <h3>Rp.150.000</h3>
+                </div>
+            </div>
+
+            <!-- ITEM 2 -->
+
+            <div class="tc-list-item">
+                <div class="tc-pic-name">
+                    <img src="../img/person-1.png">
+                    <div class="tc-list-desc-name">
+                        <h4>LPG 15 Kg</h4>
+                        <h3>Carlos Naumbabe</h3>
+                    </div>
+                </div>
+                
+                <div class="tc-label-price">
+                    <h4>Harga</h4>
+                    <h3>Rp.150.000</h3>
+                </div>
+            </div>
+
+            <!-- ITEM 3 -->
+
+            <div class="tc-list-item">
+                <div class="tc-pic-name">
+                    <img src="../img/person-2.png">
+                    <div class="tc-list-desc-name">
+                        <h4>LPG 15 Kg</h4>
+                        <h3>Sean Wijaya</h3>
+                    </div>
+                </div>
+                
+                <div class="tc-label-price">
+                    <h4>Harga</h4>
+                    <h3>Rp.150.000</h3>
+                </div>
+            </div>
+
+            <!-- ITEM 4 -->
+            <div class="tc-list-item">
+                <div class="tc-pic-name">
+                    <img src="../img/person-1.png">
+                    <div class="tc-list-desc-name">
+                        <h4>LPG 15 Kg</h4>
+                        <h3>Angga Kusumah</h3>
+                    </div>
+                </div>
+                
+                <div class="tc-label-price">
+                    <h4>Harga</h4>
+                    <h3>Rp.150.000</h3>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<!-- HISTORY SECTION -->
+
+<section class="history-section-wrapper padded">
+
+    <div class="hs-container">
+        <h2>Riwayat Tambah Saldo</h2>
+        <div class="history-list">
+            <div class="history-list-item">
+                <div class="icon-desc">
+                    <img src="../img/arrow-down.png">
+                    <div class="desc">
+                        <h4>Tambah Saldo</h4>
+                        <h4>20 Juli 2021</h4>
+                    </div>
+                </div>
+                <h3>Rp 5.000.000</h3>
+            </div>
+            <div class="history-list-item">
+                <div class="icon-desc">
+                    <img src="../img/arrow-down.png">
+                    <div class="desc">
+                        <h4>Tambah Saldo</h4>
+                        <h4>20 Juli 2021</h4>
+                    </div>
+                </div>
+                <h3>Rp 5.000.000</h3>
+            </div>
+            <div class="history-list-item">
+                <div class="icon-desc">
+                    <img src="../img/arrow-down.png">
+                    <div class="desc">
+                        <h4>Tambah Saldo</h4>
+                        <h4>20 Juli 2021</h4>
+                    </div>
+                </div>
+                <h3>Rp 5.000.000</h3>
+            </div>
+        </div>
+</section>
+
+<!-- FOOTER SECTION -->
+<section class="footer">
+    <img src="../img/icon-1.svg">
+    <img src="../img/icon-2.svg">
+    <img src="../img/icon-3.svg">
+    <img src="../img/icon-4.svg">
+    <img src="../img/icon-5.svg">
+</section>
+    
+</body>
+</html>
